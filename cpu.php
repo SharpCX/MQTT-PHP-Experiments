f2e56b83916770107358dca1c4db348dc64dcc33
Oliver Smith <chemicaloli@gmail.com>
2011-05-20 20:19:15 +0100

    Formatted

--- cpu.php
+++ cpu.php
@@ -1,24 +1,28 @@
 <?php
+
+/*
+ * A small script to get the cpu usage from a linux host and post it to an MQTT topic 
+ */
+
+
 require('SAM/php_sam.php');
 
 include('mqttConnect.php');
 
-        while($conn)
-        {
-	    
-            $cmd = "cat /proc/loadavg";
-            $rawCpu=shell_exec($cmd);
-            $result=preg_match("/^\d+.\d+/", $rawCpu, $cpu);
-            if($result==1)
-                {
-               // var_dump($cpu);
-                $msgCpu = new SAMMessage("$cpu[0]");
-                $conn->send('topic://cpu', $msgCpu, array(SAM_TIMETOLIVE=>100));
-                echo "sent";
-                sleep(60);
-            }
-                
+while ($conn)
+{
 
-        }
-
+    $cmd = "cat /proc/loadavg";
+    $rawCpu = shell_exec($cmd);
+    $result = preg_match("/^\d+.\d+/", $rawCpu, $cpu);
+    
+    if ($result == 1)
+    {
+        // var_dump($cpu);
+        $msgCpu = new SAMMessage("$cpu[0]");
+        $conn->send('topic://cpu', $msgCpu, array(SAM_TIMETOLIVE => 100));
+        echo "sent";
+        sleep(60);
+    }
+}
 ?>
