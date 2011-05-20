35c794086228235216528323aefeafc327ee326d
Oliver Smith <chemicaloli@gmail.com>
2011-05-20 20:21:09 +0100

    Formatted

--- recieve.php
+++ recieve.php
@@ -1,49 +1,54 @@
 <?php
+/*
+ * A small script which recieved the data on the roomTemp topic and broadcasts 
+ * it to pachube
+ * 
+ * @author Oliver Smith 2009
+ */
+
+
 require('SAM/php_sam.php');
-error_reporting(0); 
+error_reporting(0);
 include('mqttConnect.php');
 
 require_once( 'pachube/pachube_functions.php' );
 
-$api_key = "c1fc3113723e91e3b172355f4c7799fd4bb0e7eedef39968da009b16c0dbfa7f";
+$api_key = "<your pachube API key>";
 
 $pachube = new Pachube($api_key);
 
-$url = "http://www.pachube.com/api/4217.csv";
+$url = "<your pachube feed>";
 
+$subName = $conn->subscribe('topic://roomTemp');
 
-   
-  $subName = $conn->subscribe('topic://roomTemp');
-  
- // $dbc=sqlite_open('logger') OR die();
 $counter = 0;
-  while($conn)
-      {
+while ($conn)
+{
 
-        $msg = $conn->receive($subName,array(SAM_WAIT=>100));
+    $msg = $conn->receive($subName, array(SAM_WAIT => 100));
 
-      
 
-      if ($msg->body=="")
-      {
-      //echo "no data";
-      }
-      else
-      {
-      $counter++;
-      echo "\n".$msg->body."\n";
-	if($counter==10)
-	{
-	      echo "saved\n";
-	//	    sqlite_query($dbc, "INSERT INTO temp_rec (date, time,temp) VALUES (DATE('NOW'),TIME('NOW'),".floatval($msg->body).")");
-	$data=floatval($msg->body);
-	$update_status = $pachube->updatePachube ( $url, $data );
-	echo $update_status;
-	$counter=0;
-	}
-	    
-      }
-sleep(1);
-  }
+
+    if ($msg->body == "")
+    {
+        //echo "no data";
+    }
+    else
+    {
+        $counter++;
+        echo "\n" . $msg->body . "\n";
+        if ($counter == 10)
+        {
+            echo "saved\n";
+
+            $data = floatval($msg->body);
+            $update_status = $pachube->updatePachube($url, $data);
+
+            echo $update_status;
+            $counter = 0;
+        }
+    }
+    sleep(1);
+}
 $conn->Disconnect();
 ?>
